44f5ed4ae6687b9efad5aa31f199e22053452972
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-03-24 23:28:56 -0600

    Mostly put together! It doesn't appear to be displaying correctly on second players screen, working on that now

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -20,99 +20,150 @@ var dbref = db.ref();
 
 var p1 = false;
 var p2 = false;
-var p1Score = 0;
-var p2Score = 0;
 var player1;
 var player2;
-var userId;
+var connectedId;
+var user;
+var header = $("#header");
+var alert = $("#alert").addClass("card-title");
+var content = $("#content").addClass("card-body");
+var name;
+var p1Shoot;
+var p2Shoot;
+
+var tie = 0;
+var p1win = 0;
+var p1loss = 0;
+var p2win = 0;
+var p2loss = 0;
+var total = 0;
+
 
 var img = "./assets/images/";
-
-$("#name").focus();
-
-// * Pull prior stats
-db.ref("/stats").on("value", function (snapshot) {
-
-    p1Score = snapshot.val().p1Score;
-    p2Score = snapshot.val().p2Score;
-
-});
-
-db.ref("/users").on("value", function (snapshot) {
-
-    snapshot.val().score = p1Score;
-    snapshot.val().score = p2Score;
-    snapshot.val().name = player1;
-    snapshot.val().name = player2;
-
-});
-
-
-// * Update name for player2
-db.ref("/users").orderByChild("dateAdded").limitToFirst(2).on("child_added", function (snapshot) {
-
-    if (p1 !== p2) {
-        player2 = snapshot.val().name;
-        $("#p2").show().text(player2);
-        p2 = snapshot.key;
-
-        $("#header").hide();
-        $("#name").val("");
-
-    }
-});
-
-// * Update name for player1
-db.ref("/users").orderByChild("dateAdded").limitToFirst(1).on("child_added", function (snapshot) {
-
-    player1 = snapshot.val().name;
-    $("#p1").text(player1);
-    p1 = snapshot.key;
-    // $("#header").hide();
-    $("#p2").text("Awaiting opponent");
-    $("#name").val("");
-});
-
-
-
 var rps = ["rock", "paper", "scissors"];
 
-
-// * Disconnect settings
+// *DB connections
+var userRef = db.ref("/users");
 var myConnectionsRef = db.ref("/connected");
 var lastOnlineRef = db.ref("/lastOnline");
 var connectedRef = db.ref(".info/connected");
+
+$("#name").focus();
+
+// * Retrieve data initially
+userRef.on("value", function (snapshot) {
+
+    p1 = snapshot.val().player1.p1;
+    console.log("TCL: p1", p1);
+    p2 = snapshot.val().player2.p2;
+    console.log("TCL: p2", p2);
+    p1Shoot = snapshot.val().player1.shoot;
+    console.log("TCL: p1Shoot", p1Shoot);
+    p2Shoot = snapshot.val().player2.shoot;
+    console.log("TCL: p2Shoot", p2Shoot);
+
+});
+
+// *Game logic
+function gameTime() {
+    if (p1Shoot !== false && p2Shoot !== false) {
+        if (p1Shoot === p2Shoot) {
+            tie++;
+        } else if (p1Shoot === "paper" && p2Shoot === "rock" || p1Shoot === "rock" && p2Shoot === "scissors" || p1Shoot === "scissors" && p2Shoot === "paper") {
+            p1win++;
+            p2loss++;
+            total++;
+
+        } else if (p2Shoot === "paper" && p1Shoot === "rock" || p2Shoot === "rock" && p1Shoot === "scissors" || p2Shoot === "scissors" && p1Shoot === "paper") {
+            p2win++;
+            p1loss++;
+            total++;
+        }
+        updateScore();
+    }
+}
+
+function updateScore() {
+    db.ref("/users/stats").update({
+        tie: tie,
+        p1win: p1win,
+        p1loss: p1loss,
+        total: total,
+        p2win: p2win,
+        p2loss: p2loss,
+    });
+    clearShoot();
+}
+
+db.ref("/users/stats").on("value", function () {
+    displayScore();
+});
+
+function clearShoot() {
+    $("p1-choice").empty();
+    $("p2-choice").empty();
+    p1Shoot = false;
+    p2Shoot = false;
+    p1Shoot = db.ref("/users/player1").update({
+        shoot: false
+    });
+    p2Shoot = db.ref("/users/player2").update({
+        shoot: false
+    });
+}
+
+
+// * Disconnect settings
+
+
+// todo on connection add the specific user. User1 or user2
+
+// userRef.user.on('value', function (snapshot){
+
+
+// });
 
 connectedRef.on('value', function (snapshot) {
     if (snapshot.val() === true) {
         // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
         var con = myConnectionsRef.push();
-        userId = con.path.pieces_[1];
 
-        console.log('TCL: /users/ + userId', "/users/" + userId);
-
-        // * This isn't working, sadly
-        db.ref("/users/" + userId).onDisconnect().remove();
+        connectedId = con.path.pieces_[1];
 
         con.onDisconnect().remove();
 
-        // todo get id from disconnect and remove from user also?
-
-        // Add this device to my connections list
-        // this value could contain info about the device or a timestamp too
         con.set(true);
 
-        // When I disconnect, update the last time I was seen online
         lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
     }
 });
 
 
-
 // *Update stats section with score
-// db.ref("/stats").on("value", function (snapshot) {
 
-// });
+function displayScore() {
+
+    var newDiv = $("<div>").empty();
+    var textTies = $("<div>").text("Ties: " + tie);
+    newDiv.append(textTies);
+    $("#stats").empty().append(newDiv);
+
+    // *P1 win/losses
+    var p1Div = $("<div>").empty();
+    var p1Yes = $("<div>").text("Wins: " + p1win);
+    var p1No = $("<div>").text("Losses: " + p1loss);
+    p1Div.append(p1Yes).append(p1No);
+    $("#p1stats").empty().append(p1Div);
+
+    // *P2 win/losses
+    var p2Div = $("<div>").empty();
+    var p2Yes = $("<div>").text("Wins: " + p2win);
+    var p2No = $("<div>").text("Losses: " + p2loss);
+    p2Div.append(p2Yes).append(p2No);
+    $("#p2stats").empty().append(p2Div);
+}
+
+
 
 
 
@@ -121,23 +172,82 @@ $("#input").on("click", function (event) {
 
     event.preventDefault();
 
-    var name = $("#name").val().trim();
+    name = $("#name").val().trim();
 
-    db.ref("/users").push({
-        "name": name,
-        "userID": userId
-
-    });
-
-    // * Display RPS once input has been setup
-    if (p2 === false) {
-        displayRPS($("#p1"), "p1");
-    } else {
+    if (p1 === true && p2 === false) {
+        user = "player2";
+        console.log("TCL: user", user);
+        p2 = true;
+        
+        var player2 = {
+            p2: p2,
+            name: name,
+            connectedID: connectedId,
+            player: user,
+            shoot: false,
+        };
+        
+        db.ref("/users/stats").set({
+            tie: 0,
+            p1win: 0,
+            p1loss: 0,
+            total: 0,
+            p2win: 0,
+            p2loss: 0,
+        });
+        
+        // * Display RPS once input has been setup
         displayRPS($("#p2"), "p2");
+        
+        // * Set new location for this user
+        db.ref("/users/" + user).set(player2);
+        db.ref("/users/" + user).onDisconnect().remove();
+        db.ref("/users/" + user).onDisconnect(function () {
+            db.ref("/users/stats").remove();
+        });
+        alert.text("Rock, Paper, Scissors...SHOOT!");
+        header.hide();
+
+    } else if ((p1 === false && p2 === true) || (p1 === false && p2 === false)) {
+        user = "player1";
+        console.log("TCL: user", user);
+        p1 = true;
+        
+        var player1 = {
+            p1: p1,
+            name: name,
+            connectedID: connectedId,
+            player: user,
+            shoot: false,
+        };
+        
+        // * Display RPS once input has been setup
+        displayRPS($("#p1"), "p1");
+
+        db.ref("/users/stats").set({
+            tie: 0,
+            p1win: 0,
+            p1loss: 0,
+            total: 0,
+            p2win: 0,
+            p2loss: 0,
+        });
+
+        // * Set new location for this user
+        db.ref("/users/" + user).set(player1);
+        db.ref("/users/" + user).onDisconnect().remove();
+        db.ref("/users/" + user).onDisconnect(function () {
+            db.ref("/users/stats").remove();
+        });
+        header.hide();
+        alert.text("Rock, Paper, Scissors...SHOOT!");
+
+    } else {
+        alert.text("Seats taken, you may spectate");
     }
 
 
-    // if ()
+
 
 });
 
@@ -160,18 +270,38 @@ function displayRPS(location, user) {
 $("#p1").on("click", ".rps", function () {
     var shoot = $(this).val();
 
-    db.ref("users/" + p1).update({
+    db.ref("users/" + user).update({
         "shoot": shoot
     });
     $("#p1-choice").empty().append("<img src='" + img + shoot + ".png'>");
+    gameTime();
 });
 
 
 $("#p2").on("click", ".rps", function () {
     var shoot = $(this).val();
 
-    db.ref("users/" + p2).update({
+    db.ref("users/" + user).update({
         "shoot": shoot
     });
     $("#p2-choice").empty().append("<img src='" + img + shoot + ".png' >");
+    gameTime();
+});
+
+$("#reset").on("click", function () {
+    userRef.remove();
+    header.show();
+    $("#p1").empty();
+    $("#p1-choice").empty();
+    $("#p2").empty();
+    $("#p2-choice").empty();
+    alert.text("Choose a name");
+    content.text("");
+    p1 = false;
+    p2 = false;
+    tie = 0;
+    p1win = 0;
+    p1loss = 0;
+    p2win = 0;
+    p2loss = 0;
 });
